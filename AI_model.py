d780c5184a41d1e842c94f10c1e12745c6e05158
YashNarayan-thefirst <106808693+YashNarayan-thefirst@users.noreply.github.com>
2025-09-27 10:04:31 -0400

    added chatgpt

---
+++ AI_model.py
@@ -0,0 +1,16 @@
+from typing import Any
+from openai import OpenAI
+
+def generate_test_plan(data: dict[Any,Any],model:str,api_key:str):
+    client = OpenAI(api_key=api_key,base_url = "https://api.deepseek.com")
+    structured_data = str(data)
+    prompt = f""" You are a an expert. You are writing a test plan for the model. Please build a plan using {structured_data}. """
+    repsonse = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ],
+        stream = False
+    )
+    return repsonse.choices[0].message.content
