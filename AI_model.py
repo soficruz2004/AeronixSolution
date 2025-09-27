29a0b69e9a237161d6892a8716ba44c0b4deccde
YashNarayan-thefirst <106808693+YashNarayan-thefirst@users.noreply.github.com>
2025-09-27 11:39:13 -0400

    added better parser

--- AI_model.py
+++ AI_model.py
@@ -10,7 +10,7 @@ api_key = os.getenv("API_KEY")
 def generate_test_plan(data: dict[Any,Any],model:str ="deepseek/deepseek-chat-v3.1:free") -> Optional[str|None]:
     client = OpenAI(api_key=api_key,base_url = "https://openrouter.ai/api/v1")
     structured_data = str(data)
-    prompt = f""" You are an expert in PCB testing and design. Please build a study plan using {structured_data}."""
+    prompt = f""" You are an expert in PCB testing and design. Please develop tests for {structured_data}."""
     repsonse = client.chat.completions.create(
         model=model,
         messages=[
