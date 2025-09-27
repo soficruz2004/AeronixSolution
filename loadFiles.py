a870b2711748ed2cba4b230a83aeabbc48403643
YashNarayan-thefirst <106808693+YashNarayan-thefirst@users.noreply.github.com>
2025-09-27 09:52:12 -0400

    Added files

--- loadFiles.py
+++ loadFiles.py
@@ -1,9 +1,16 @@
 import os
+from typing import Any
 
-def load_inputs():
+def load_inputs() -> list[str]:
     files:list[str] = []
     for file in os.listdir("inputs"):
-        for i in ["netlist", "bom", "schematic", "req_doc"]:
-            if file.endswith(i): files.append(os.path.join("inputs", file))
+        if file.endswith(".ipc"): files.append(os.path.join("inputs", file))
     assert len(files) > 0, "No input files specified."
     return files
+
+def parse_inputs(files: list[str])->dict[Any,Any]:
+    parsed:dict[Any,Any] = {}
+    for file in file:
+        if 
+
+    return parsed
